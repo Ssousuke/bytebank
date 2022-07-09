b5b1ded65b10988f9db2911fa9237743648456a6
Wesley Farias <wesleysousukee@gmail.com>
2022-07-09 02:55:41 -0300

    implementa property

--- bytebank/models/ContaCorrente.cs
+++ bytebank/models/ContaCorrente.cs
@@ -6,7 +6,8 @@
         public string Conta { get; }
         public string Agencia { get; }
         public int NumeroAgencia { get; }
-        public double Saldo { get; set; }
+        public double Saldo { get; private set; }
+        public static int TotalDeContas { get; private set; }
 
         public ContaCorrente(Cliente titular, string conta, string agencia, int numeroAgencia)
         {
@@ -14,7 +15,7 @@
             Conta = conta;
             Agencia = agencia;
             NumeroAgencia = numeroAgencia;
-            Saldo = 0.0;
+            TotalDeContas += 1;
         }
 
         public bool Saca(double valor)
@@ -27,11 +28,9 @@
             {
                 return false;
             }
-            else
-            {
-                Saldo -= valor;
-                return true;
-            }
+
+            Saldo -= valor;
+            return true;
         }
 
         public void Deposita(double valor)
@@ -57,7 +56,7 @@
             }
 
             conta.Deposita(valor);
-            this.Saca(valor);
+            Saca(valor);
             return true;
         }
 
