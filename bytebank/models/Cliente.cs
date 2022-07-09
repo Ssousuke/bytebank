739c4f34bd71a030ba7e1deecf74fd3b6a34c70a
Wesley Farias <wesleysousukee@gmail.com>
2022-07-09 02:13:20 -0300

    add Cliente

---
+++ bytebank/models/Cliente.cs
@@ -0,0 +1,23 @@
+﻿namespace bytebank.models;
+
+public class Cliente
+{
+    public string TitularNome { get; }
+    public string TitularCpf { get; }
+    public string TitularProfissao { get; }
+
+
+    public Cliente(string titularNome, string titularCpf, string titularProfissao)
+    {
+        TitularNome = titularNome;
+        TitularCpf = titularCpf;
+        TitularProfissao = titularProfissao;
+    }
+
+    public override string ToString()
+    {
+        return "Nome: " + TitularNome +
+               ", CPF: " + TitularCpf +
+               ", Profissão: " + TitularProfissao;
+    }
+}
